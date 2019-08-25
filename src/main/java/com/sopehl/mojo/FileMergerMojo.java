49a7630adefb9661b6ef1ce91ec50f0dcb711c7e
sopehl <semihokanp@gmail.com>
2019-08-25 11:24:26 +0300

    Removed unused imports

--- src/main/java/com/sopehl/mojo/FileMergerMojo.java
+++ src/main/java/com/sopehl/mojo/FileMergerMojo.java
@@ -6,19 +6,13 @@ import com.sopehl.spec.Writeable;
 import com.sopehl.util.FileUtils;
 import com.sopehl.util.Generator;
 import com.sopehl.util.VersionUtils;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
 import java.util.List;
 
 @Mojo(name = "merge")

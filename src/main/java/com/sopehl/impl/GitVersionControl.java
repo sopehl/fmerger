ca689604669c818c26104aaddef90ec9d8b8e5fa
sopehl <semihokanp@gmail.com>
2019-12-11 11:02:14 +0300

    Changed add index command #25

--- src/main/java/com/sopehl/impl/GitVersionControl.java
+++ src/main/java/com/sopehl/impl/GitVersionControl.java
@@ -9,7 +9,7 @@ public class GitVersionControl implements VersionControl {
     @Override
     public void commitAndPush(Scm scm) {
         ProcessUtils.execute("git status");
-        ProcessUtils.execute("git add .");
+        ProcessUtils.execute("git add --all");
         ProcessUtils.execute("git commit -m " + scm.getCommentPrefix());
         ProcessUtils.execute("git push " + scm.getConnection() + " refs/heads/master:refs/heads/master");
     }
